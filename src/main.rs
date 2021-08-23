9bf479f4f56037cd0ae3fdcb0c76eb6afaffe5fe
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-08-23 19:01:09 +0200

    structs #2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
+  let mut user_vec: Vec<User> = Vec::new();
+
   let user_1 = User {
     username: "Tim1337".to_string(),
     password: "s3cr3t!".to_string(),
     login_count: 0,
   };
+
+  // One line only
+  println!("[DEBUG] user_1 is: {:?}", user_1);
+  // Span over multiple lines
+  println!("[DEBUG] user_1 is: {:#?}", user_1);
 
   let user_2 = User {
     username: "kekw_h4ckz0r".to_string(),
@@ -11,17 +18,24 @@ fn main() {
     ..user_1
   };
 
-  println!(
-    "{}:\npassword: {}\nlogin count: {}",
-    user_1.username, user_1.password, user_1.login_count
-  );
+  user_vec.push(user_1);
+  user_vec.push(user_2);
 
-  println!(
-    "{}:\npassword: {}\nlogin count: {}",
-    user_2.username, user_2.password, user_2.login_count
-  );
+  for u in user_vec.iter() {
+    print_user(u);
+  }
 }
 
+fn print_user(user: &User) {
+  println!("==============================");
+  println!(
+    "Printing out information for user {}:\npassword: {}\nlogin count: {}",
+    user.username, user.password, user.login_count
+  );
+  println!("==============================");
+}
+
+#[derive(Debug)]
 struct User {
   username: String,
   password: String,
