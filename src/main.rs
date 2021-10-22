365c833fc3b83f82febb4c4f7662547378907ce4
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-10-22 20:35:29 +0200

    Option<T> enum (Some, None)

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,7 @@
 fn main() {
-  let mut user_vec: Vec<User> = Vec::new();
+  let x = 5;
 
-  let user_1 = User {
-    username: "Tim1337".to_string(),
-    password: "s3cr3t!".to_string(),
-    login_count: 0,
-  };
+  let y: Option<i32> = Some(5);
 
-  // One line only
-  println!("[DEBUG] user_1 is: {:?}", user_1);
-  // Span over multiple lines
-  println!("[DEBUG] user_1 is: {:#?}", user_1);
-
-  let user_2 = User {
-    username: "kekw_h4ckz0r".to_string(),
-    password: "123".to_string(),
-    ..user_1
-  };
-
-  user_vec.push(user_1);
-  user_vec.push(user_2);
-
-  for u in user_vec.iter() {
-    print_user(u);
-  }
-}
-
-fn print_user(user: &User) {
-  println!("==============================");
-  println!(
-    "Printing out information for user {}:\npassword: {}\nlogin count: {}",
-    user.username, user.password, user.login_count
-  );
-  println!("==============================");
-}
-
-#[derive(Debug)]
-struct User {
-  username: String,
-  password: String,
-  login_count: u64,
+  println!("{}", x + y.unwrap());
 }
