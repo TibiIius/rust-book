ea1e4749554c6bafc1f4f9f5701b6e6e7cc7cc1e
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-10-23 00:58:00 +0200

    fixup! enums and matches

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 fn main() {
   let my_coin: Coin = Coin::Nickel;
 
-  println!("{}", Coin::value_coin_cents(&my_coin));
+  let my_special_coin = Coin::Special(30);
+
+  println!(
+    "My coin is worth {}, my special coin is worth {} cents.",
+    Coin::value_coin_cents(&my_coin),
+    Coin::value_coin_cents(&my_special_coin)
+  );
 }
 
 enum Coin {
   Penny,
   Nickel,
   Dime,
+  Special(u8),
 }
 
 impl Coin {
@@ -16,6 +23,7 @@ impl Coin {
       Coin::Penny => 2,
       Coin::Nickel => 5,
       Coin::Dime => 10,
+      Coin::Special(s) => *s,
     }
   }
 }
