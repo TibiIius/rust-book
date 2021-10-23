cdae344e230a9a9ce5ef049a81b70958e45c7440
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-10-23 02:31:08 +0200

    match + if let

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
   let my_coin: Coin = Coin::Nickel;
-
   let my_special_coin = Coin::Special(30);
 
   println!(
@@ -8,6 +7,29 @@ fn main() {
     Coin::value_coin_cents(&my_coin),
     Coin::value_coin_cents(&my_special_coin)
   );
+
+  let maybe_int = square_val(&Some(5));
+  let not_int = square_val(&None);
+
+  let maybe_int_il: Option<i32> = square_val_iflet(&Some(5));
+  let not_int_il: Option<i32> = square_val_iflet(&None);
+
+  std::process::exit(0);
+}
+
+fn square_val(&x: &Option<i32>) -> Option<i32> {
+  match x {
+    Some(v) => Some(v * v),
+    _ => None,
+  }
+}
+
+fn square_val_iflet(&x: &Option<i32>) -> Option<i32> {
+  if let (Some(v)) = x {
+    Some(v * v)
+  } else {
+    None
+  }
 }
 
 enum Coin {
