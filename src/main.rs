031ab419bed98ba4aaf3e43d1091bd125246a3cb
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-11-09 02:10:56 +0100

    chapter 10: generics

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
-use std::fs::File;
-use std::io::ErrorKind;
-
 fn main() {
-  let f = File::open("./test.md");
+  let number_list = vec![1, 2, 3, 4];
 
-  let f = match f {
-    Ok(f) => f,
-    Err(e) => match e.kind() {
-      ErrorKind::NotFound => {
-        println!("File does not yet exist and will be created");
-        match File::create("./test.md") {
-          Ok(fc) => fc,
-          Err(e) => panic!("Error creating file: {:?}", e),
-        }
-      }
-      other => panic!("Error reading/creating file: {:?}", other),
-    },
-  };
+  let char_list = vec!['a', 'b', 'C'];
+
+  println!("the largest number in the list is: {}", largest(&number_list));
+
+  println!("the largest char in the list is: {}", largest(&char_list));
+
+  let point = Point { x: 5, y: 7 };
+  let point_other = Point { x: 5, y: 7 };
+
+  println!("it is {} that both points are equal.", point.equals(&point_other));
+}
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> T
+where
+  T: Copy,
+{
+  let mut largest = list[0];
+
+  for &i in list {
+    if i > largest {
+      largest = i;
+    }
+  }
+
+  largest
+}
+
+struct Point<T: std::cmp::PartialEq> {
+  x: T,
+  y: T,
+}
+
+impl<T: std::cmp::PartialEq> Point<T> {
+  fn equals(&self, other: &Point<T>) -> bool {
+    self.y == other.y && self.x == other.x
+  }
 }
