a7188a144b3d8b4147dff047bfc064eb5d5c8585
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-11-21 03:54:41 +0100

    chapter 12: minigrep stderror

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use minigrep::Arguments;
 
 fn main() {
   let parsed_args = Arguments::new(std::env::args().collect::<Vec<String>>().as_ref()).unwrap_or_else(|e| {
-    println!("Error parsing console arguments: {}", e);
+    eprintln!("Error parsing console arguments: {}", e);
     std::process::exit(1);
   });
 
   // Use if let instead of unwrap_or_else() because we don't want to unwrap Ok(()), we only care about the error value
   if let Err(e) = minigrep::run(parsed_args) {
-    println!("The program encountered an unexpected error: {}", e);
+    eprintln!("The program encountered an unexpected error: {}", e);
 
     std::process::exit(1);
   }
