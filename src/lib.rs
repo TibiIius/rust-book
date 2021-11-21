08b21210dff73d36641f55e3bafa66ea28a3fa55
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-11-21 01:48:18 +0100

    chapter 12: finish minigrep

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,59 @@ impl Arguments {
 }
 
 pub fn run(parsed_args: Arguments) -> Result<(), Box<dyn Error>> {
-  println!(
-    "Searching for \"{}\" in file \"{}\"",
-    parsed_args.query_string, parsed_args.file_name
-  );
-
   let contents = fs::read_to_string(parsed_args.file_name)?;
 
-  println!("The file consists of the following:\n{}", contents);
+  for l in find_lines(parsed_args.query_string.as_ref(), contents.as_ref()) {
+    println!("{}", l);
+  }
 
   Ok(())
+}
+
+pub fn find_lines<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+  let mut ret: Vec<&str> = vec![];
+
+  for l in content.lines() {
+    if l.contains(query) {
+      ret.push(l);
+    }
+  }
+
+  ret
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn new_arguments() {
+    let args = vec!["program_name".to_string(), "test".to_string(), "file.md".to_string()];
+
+    let args_parsed = Arguments::new(args.as_ref());
+
+    assert_eq!(args_parsed.as_ref().unwrap().query_string, "test".to_string());
+    assert_eq!(args_parsed.as_ref().unwrap().file_name, "file.md".to_string());
+  }
+
+  #[test]
+  fn new_arguments_incorrect() {
+    let args = vec!["program_name".to_string()];
+
+    let args_parsed = Arguments::new(args.as_ref());
+
+    assert!(args_parsed.is_err());
+  }
+
+  #[test]
+  fn single_result() {
+    let query = "duct";
+
+    let file_content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive."], find_lines(query, file_content));
+  }
 }
