55e79c17024a695a133442f06a3c4fb1d6d8107b
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-06 22:02:23 +0100

    chapter 13: mutable borrow

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
 mkShell {
-  buildInputs = [ cargo rustc rustfmt rustPackages.clippy rust-analyzer ];
+  buildInputs = [ cargo rustc rustfmt clippy rust-analyzer ];
   RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-
 fn main() {
-  let list = vec![1,2,3];
+  let mut list = vec![1, 2, 3];
   println!("Before defining closure: {:?}", list);
 
-  let only_borrows = || println!("From closure {:?}", list);
+  let mut borrows_mutably = || list.push(7);
 
-  println!("Before calling closure: {:?}", list);
-  only_borrows();
+  // Cannot be called as the closure borrows the list mutably!
+  // println!("Before calling closure: {:?}", list);
+
+  borrows_mutably();
   println!("After calling closure: {:?}", list);
 }
