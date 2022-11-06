e47391f9fe3ccd724156dd676e3dc5994884bd11
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-06 18:30:18 +0100

    add nix flake support

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      });
+}
