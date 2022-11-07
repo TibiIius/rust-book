060f9dace0997ce055bbb9d1c2e99971d719656b
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-07 02:43:52 +0100

    chapter 13: iters 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-fn main() {
-  let list: Vec<i32> = vec![1, 2, 3];
-
-  println!("Original list:");
-  for val in list.iter() {
-    print!("{} ", val);
-  }
-  println!();
-
-  let list2: Vec<_> = list.iter().map(|x| x + 1).collect();
-
-  println!("Modified list:");
-  for val in list2.iter() {
-    print!("{} ", val);
-  }
-  println!();
-}
+fn main() {}
