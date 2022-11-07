d54066155ee2f7b2d81c54ffbe7f6afb49944345
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-07 21:36:09 +0100

    chapter 15: box recursive type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-fn main() {
-  let b = Box::new(5);
+// Can't compile like this
+// Reason is that Rust tries to find the largest possible value in the enum, and since `Cons(i32, List)` can be infinitely big, the size can't be determined
+// enum List {
+//   Cons(i32, List),
+//   Nil,
+// }
 
-  println!("b = {b}");
+enum List {
+  Cons(i32, Box<List>),
+  Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+  let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+  let x = 5;
+  let y = Box::new(x);
+
+  assert_eq!(5, x);
+  assert_eq!(5, *y);
 }
