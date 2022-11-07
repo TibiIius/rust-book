8a2aa491d91cac25bd2e96fcb73babd96f3b94a5
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-07 01:03:40 +0100

    some nix stuff

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
 mkShell {
-  buildInputs = [ cargo rustc rustfmt clippy rust-analyzer ];
-  RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+  buildInputs = [ cargo rustc rustfmt clippy rust-analyzer nixpkgs-fmt ];
+  shellHook = ''
+    export RUST_SRC_PATH="${rust.packages.stable.rustPlatform.rustLibSrc}"
+  '';
 }
