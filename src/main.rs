237054d558e811d6b4de7822bd6e09b56a632bf2
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-07 22:50:47 +0100

    chapter 15: custom box type

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-// Can't compile like this
-// Reason is that Rust tries to find the largest possible value in the enum, and since `Cons(i32, List)` can be infinitely big, the size can't be determined
-// enum List {
-//   Cons(i32, List),
-//   Nil,
-// }
+use std::ops::Deref;
+struct MyBox<T>(T);
 
-enum List {
-  Cons(i32, Box<List>),
-  Nil,
+impl<T> Deref for MyBox<T> {
+  type Target = T;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
 }
 
-use crate::List::{Cons, Nil};
-
-fn main() {
-  let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-
-  let x = 5;
-  let y = Box::new(x);
-
-  assert_eq!(5, x);
-  assert_eq!(5, *y);
-}
+fn main() {}
