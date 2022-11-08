b70d01f9a9cc6e2540a6b6133c1230e3638287c6
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-09 00:24:50 +0100

    chapter 15: Rc

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
-struct CustomSmartPointer {
-  data: String,
-}
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
-impl Drop for CustomSmartPointer {
-  fn drop(&mut self) {
-    println!("Dropping CustomSmartPointer with data: `{}`.", self.data);
-  }
+// this won't work as a would be moved into b and c, and can't have multiple owners
+// enum List {
+//   Cons(i32, Box<List>),
+//   Nil,
+// }
+
+// fn main() {
+// let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+// let b = Cons(3, Box::new(a));
+// let c = Cons(4, Box::new(a));
+// }
+
+enum List {
+  Cons(i32, Rc<List>),
+  Nil,
 }
 
 fn main() {
-  let c = CustomSmartPointer {
-    data: String::from("my stuff"),
-  };
-
-  let c2 = CustomSmartPointer {
-    data: String::from("other stuff"),
-  };
-
-  println!("CustomSmartPointer instances created.");
-
-  std::mem::drop(c2);
-  println!("c2 dropped before end of main.");
+  let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+  println!("count after creating a = {}", Rc::strong_count(&a));
+  // Rc::clone() is not a deep copy, it only increases the ref count
+  let b = Cons(3, Rc::clone(&a));
+  println!("count after creating a = {}", Rc::strong_count(&a));
+  {
+    let c = Cons(4, Rc::clone(&a));
+    println!("count after creating c = {}", Rc::strong_count(&a));
+  }
+  println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
