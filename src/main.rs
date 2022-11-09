3e6a703e3187b92a816eac0681aa1f6cce49c610
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-09 01:13:20 +0100

    chapter 15: RefCell

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
 use crate::List::{Cons, Nil};
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
-// this won't work as a would be moved into b and c, and can't have multiple owners
-// enum List {
-//   Cons(i32, Box<List>),
-//   Nil,
-// }
-
-// fn main() {
-// let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-// let b = Cons(3, Box::new(a));
-// let c = Cons(4, Box::new(a));
-// }
-
+#[derive(Debug)]
 enum List {
-  Cons(i32, Rc<List>),
+  Cons(Rc<RefCell<i32>>, Rc<List>),
   Nil,
 }
 
 fn main() {
-  let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-  println!("count after creating a = {}", Rc::strong_count(&a));
-  // Rc::clone() is not a deep copy, it only increases the ref count
-  let b = Cons(3, Rc::clone(&a));
-  println!("count after creating a = {}", Rc::strong_count(&a));
-  {
-    let c = Cons(4, Rc::clone(&a));
-    println!("count after creating c = {}", Rc::strong_count(&a));
-  }
-  println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+  let value = Rc::new(RefCell::new(5));
+
+  let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+  let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+  let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+  *value.borrow_mut() += 10;
+
+  println!("a after = {:?}", a);
+  println!("b after = {:?}", b);
+  println!("c after = {:?}", c);
 }
