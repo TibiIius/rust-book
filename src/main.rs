6c9a59c06a0a66e53e4a58a9dafbb6e33696b020
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-09 02:01:51 +0100

    chapter 15: Weak

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
-use crate::List::{Cons, Nil};
-use std::{cell::RefCell, rc::Rc};
+use std::{
+  borrow::{Borrow, BorrowMut},
+  cell::RefCell,
+  rc::{Rc, Weak},
+};
 
 #[derive(Debug)]
-enum List {
-  Cons(Rc<RefCell<i32>>, Rc<List>),
-  Nil,
+struct Node {
+  value: i32,
+  parent: RefCell<Weak<Node>>,
+  children: RefCell<Vec<Rc<Node>>>,
 }
 
 fn main() {
-  let value = Rc::new(RefCell::new(5));
+  let leaf = Rc::new(Node {
+    value: 3,
+    parent: RefCell::new(Weak::new()),
+    children: RefCell::new(vec![]),
+  });
 
-  let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+  println!("Leaf parent = {:?}", leaf.parent.borrow().upgrade());
+  println!(
+    "leaf strong = {}, weak = {}",
+    Rc::strong_count(&leaf),
+    Rc::weak_count(&leaf),
+  );
 
-  let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-  let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+  {
+    let branch = Rc::new(Node {
+      value: 5,
+      parent: RefCell::new(Weak::new()),
+      children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
 
-  *value.borrow_mut() += 10;
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 
-  println!("a after = {:?}", a);
-  println!("b after = {:?}", b);
-  println!("c after = {:?}", c);
+    println!(
+      "branch strong = {}, weak = {}",
+      Rc::strong_count(&branch),
+      Rc::weak_count(&branch),
+    );
+
+    println!(
+      "leaf strong = {}, weak = {}",
+      Rc::strong_count(&leaf),
+      Rc::weak_count(&leaf),
+    );
+  }
+
+  println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+  println!(
+    "leaf strong = {}, weak = {}",
+    Rc::strong_count(&leaf),
+    Rc::weak_count(&leaf),
+  );
 }
