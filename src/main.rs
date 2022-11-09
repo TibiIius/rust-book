e7bea979b1d2e06c09ef9c061e972cdec14ba881
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-09 23:32:13 +0100

    chapter 16: basic mutex example

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,19 @@
-use std::{process, sync::mpsc, thread, time::Duration};
+use std::{
+  process,
+  sync::{mpsc, Mutex},
+  thread,
+  time::Duration,
+};
 
 fn main() {
-  let (tx, rx) = mpsc::channel();
-  let tx2 = tx.clone();
+  let mtx = Mutex::new(5);
 
-  thread::spawn(move || {
-    let vals = vec![String::from("Hello"), String::from("from"), String::from("sender.")];
-
-    for val in vals {
-      if let Err(e) = tx.send(val) {
-        println!("An error occured trying to send some data to a receiver: {e}");
-      }
-      thread::sleep(Duration::from_secs(1));
-    }
-  });
-
-  thread::spawn(move || {
-    let vals = vec![String::from("How"), String::from("are"), String::from("you?")];
-
-    for val in vals {
-      if let Err(e) = tx2.send(val) {
-        println!("An error occured trying to send some data to a receiver: {e}");
-      }
-      thread::sleep(Duration::from_secs(1));
-    }
-  });
-
-  // `recv()` blocks until the transmitter sent something
-  // `try_recv()` would not block and return Err if it got nothing from the transmitter
-  // let received = rx.recv().unwrap();
-
-  // basically calls recv() a couple of times
-  for received in rx {
-    println!("Received some data from another thread: {received}");
+  {
+    // `lock()` tries to acquire the mutex' lock
+    // if the mutex is locked, it blocks until it can acquire the lock
+    let mut guard = mtx.lock().unwrap();
+    *guard = 6;
   }
 
-  process::exit(0)
+  println!("mtx = {:?}", mtx);
 }
