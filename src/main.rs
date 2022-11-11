171f006939d50080f649b0824d425d529860c043
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-11 16:40:20 +0100

    chapter 18: pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,51 @@
-use blog::Post;
+struct Point {
+  x: i32,
+  y: i32,
+}
 
 fn main() {
-  let mut post = Post::new();
+  let mut stack = Vec::new();
 
-  post.add_text("I ate a salad for lunch today");
-  assert_eq!("", post.content());
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
 
-  post.request_review();
-  assert_eq!("", post.content());
+  //`let Some(x)` -> refutable pattern
+  // only possible with `if let` and `while let`
+  while let Some(e) = stack.pop() {
+    println!("{e}");
+  }
 
-  post.reject();
-  post.request_review();
+  let my_vec = vec!["one", "two", "three"];
 
-  post.approve();
-  post.approve();
-  assert_eq!("I ate a salad for lunch today", post.content());
+  for (index, val) in my_vec.iter().enumerate() {
+    println!("{index}: {val}");
+  }
+
+  let x = Some(5);
+
+  print!("x is ");
+  match x {
+    Some(5) => println!("five"),
+    Some(4 | 6) => println!("four or six"),
+    Some(10..=100000) => println!("a big number (between 10 and 100000)"),
+    // Some(y) => println!("matched something else: {y}"), // Same as below, but the matched variable is not dropped and instead is bound to y
+    Some(_) => println!("something else"),
+    None => println!("nothing"),
+  }
+
+  let point = Point { x: 0, y: 7 };
+
+  // destruct so that we can use the variables x and y directly
+  let Point { x, y } = point;
+  assert_eq!(0, x);
+  assert_eq!(7, y);
+
+  let numbers = (2, 4, 8, 16, 32);
+
+  match numbers {
+    (first, .., last) => {
+      println!("Some numbers: {first}, {last}");
+    }
+  }
 }
