b3157faa1591ce7d5141f85ad33ccd2a324af219
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-13 22:31:45 +0100

    chapter 19: methods with the same name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,55 @@
-// unsafe traits hold methods that may contain unsafe code
-unsafe trait IAmUnsafe {}
+struct Human;
 
-unsafe impl IAmUnsafe for i32 {}
+struct Dog;
 
-fn main() {}
+trait Pilot {
+  fn fly(&self);
+}
+
+trait Wizard {
+  fn fly(&self);
+}
+
+trait Animal {
+  fn baby_name() -> String;
+}
+
+impl Human {
+  fn fly(&self) {
+    println!("*waving arms furiously*");
+  }
+}
+
+impl Pilot for Human {
+  fn fly(&self) {
+    println!("This is your captain speaking.");
+  }
+}
+
+impl Wizard for Human {
+  fn fly(&self) {
+    println!("We Morrowind now.");
+  }
+}
+
+impl Dog {
+  fn baby_name() -> String {
+    String::from("Spot")
+  }
+}
+
+impl Animal for Dog {
+  fn baby_name() -> String {
+    String::from("puppy")
+  }
+}
+
+fn main() {
+  let person = Human;
+  person.fly();
+  Pilot::fly(&person);
+  Wizard::fly(&person);
+
+  println!("A name for a baby dog could be \"{}.\"", Dog::baby_name());
+  println!("A baby dog is called a {}.", <Dog as Animal>::baby_name());
+}
