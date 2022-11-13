ad144715b74b25e63bca5767e001ac9334864dac
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-13 23:03:20 +0100

    chapter 19: supertrait (trait within another trait)

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,32 @@
-struct Human;
+use std::fmt;
 
-struct Dog;
-
-trait Pilot {
-  fn fly(&self);
-}
-
-trait Wizard {
-  fn fly(&self);
-}
-
-trait Animal {
-  fn baby_name() -> String;
-}
-
-impl Human {
-  fn fly(&self) {
-    println!("*waving arms furiously*");
+trait OutlinePrint: fmt::Display {
+  // make nice output :)
+  fn outline_print(&self) {
+    let output = self.to_string();
+    let len = output.len();
+    println!("{}", "*".repeat(len + 4));
+    println!("*{}*", " ".repeat(len + 2));
+    println!("* {} *", output);
+    println!("*{}*", " ".repeat(len + 2));
+    println!("{}", "*".repeat(len + 4));
   }
 }
 
-impl Pilot for Human {
-  fn fly(&self) {
-    println!("This is your captain speaking.");
+struct Point {
+  x: i32,
+  y: i32,
+}
+
+impl fmt::Display for Point {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "({}, {})", self.x, self.y)
   }
 }
 
-impl Wizard for Human {
-  fn fly(&self) {
-    println!("We Morrowind now.");
-  }
-}
-
-impl Dog {
-  fn baby_name() -> String {
-    String::from("Spot")
-  }
-}
-
-impl Animal for Dog {
-  fn baby_name() -> String {
-    String::from("puppy")
-  }
-}
+impl OutlinePrint for Point {}
 
 fn main() {
-  let person = Human;
-  person.fly();
-  Pilot::fly(&person);
-  Wizard::fly(&person);
-
-  println!("A name for a baby dog could be \"{}.\"", Dog::baby_name());
-  println!("A baby dog is called a {}.", <Dog as Animal>::baby_name());
+  let p = Point { x: 1, y: 2 };
+  p.outline_print();
 }
