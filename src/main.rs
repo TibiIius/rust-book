83ac5f577c2316c19d2a774cb74c89298f250c40
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-13 22:23:09 +0100

    chapter 19: unsafe traits

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-// static variables are basically like constants
-// unlike constants though, they always refer to the same value in memory, meaning they can never be copied
-// accessing static mutable variables is considered unsafe
-// they are considered unsafe as mutiple parts of the program always access the same instance
-// with mutiple threads, accessing static variables and mutating them can lead to data races
-static mut COUNTER: u32 = 0;
+// unsafe traits hold methods that may contain unsafe code
+unsafe trait IAmUnsafe {}
 
-fn add_to(inc: u32) {
-  unsafe {
-    COUNTER += inc;
-  }
-}
-fn main() {
-  add_to(3);
+unsafe impl IAmUnsafe for i32 {}
 
-  unsafe {
-    println!("COUNTER has the value: {COUNTER}");
-  }
-}
+fn main() {}
