1034f791bd83197eb9321160fa4fb4cb9974b515
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-15 15:16:59 +0100

    chapter 19: declarative macro

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-// this returns a closure
-fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
-  Box::new(|x| x + 1)
+// make macro available to crates bringing this crate into scope
+#[macro_export]
+macro_rules! vec {
+  // `$` -> declares a macro variable
+  // `expr` matches any Rust expression, and `$x:expr` assings the Rust expression to the macro variable `$x`
+  // the `,` is just literally a comma in the expression
+  // the `*` indicates to match the preceding pattern 0 or more times
+  ( $( $x:expr ),* ) => {
+    {
+      let mut temp_vec = Vec::new();
+      // `$()*` generates code for each repetition of the pattern
+      $(
+        temp_vec.push($x);
+      )*
+      temp_vec
+    }
+  };
 }
 
-fn main() {}
+fn main() {
+  let t = vec![1, 2, 3];
+}
